4f28804ad8a221668c0d70f98362c99c55d65287
sivakumarsetti <sivakumarsettii@gmail.com>
2024-12-06 19:44:55 +0530

    latest

--- common/dbConnection.js
+++ common/dbConnection.js
@@ -1,7 +1,7 @@
 const mongodb = require("mongodb")
 
 async function getDb(){
-    var url = 'mongodb+srv://siva:siva@cluster0.kuldp.mongodb.net/'
+    var url = 'mongodb+srv://siva:siva@cluster1.aldep.mongodb.net/'
     var mongoClient = mongodb.MongoClient
     var server = await mongoClient.connect(url)
     var db = server.db("7am")
